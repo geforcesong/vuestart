ce81a8bd1ecd35ce9133b55ebdb3b6d84164eacf
gguo <gguo@movoto.com>
2016-09-10 14:31:32 +0800

    add test

--- generatorSample.js
+++ generatorSample.js
@@ -1,19 +1,23 @@
-var co = require("co");
-co(function* () {
-  var result = yield asyncFun(1);
-  var result1 = yield asyncFun(2);
-  var d = yield [asyncFun(3), asyncFun(4)];
-  return d;
-}).then(function (value) {
-  console.log(value);
-}, function (err) {
-  console.error(err.stack);
-});
+var co = require('co')
+co(function*() {
+    var result = yield asyncFun(1)
+    var result1 = yield asyncFun(2)
+    var d = yield [asyncFun(3), asyncFun(4)]
+    return d
+}).then(function(value) {
+    console.log(value)
+}, function(err) {
+    console.error(err.stack)
+})
 
-function asyncFun(v){
-    return new Promise((resolve, reject)=>{
+function asyncFun(v) {
+    return new Promise((resolve, reject) => {
         setTimeout(function() {
-            return resolve(v*10);
-        }, 500);
+            return resolve(v * 10)
+        }, 500)
     })
+}
+
+function c() {
+    console.log("hello");
 }
