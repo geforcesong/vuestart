53fd64c4d5e8b480d89791e097dda28a7d5da0c1
gguo <gguo@movoto.com>
2016-09-24 14:24:34 +0800

    cm

--- generatorSample.js
+++ generatorSample.js
@@ -1,18 +1,18 @@
 var co = require('co')
-co(function*() {
+co(function* () {
     var result = yield asyncFun1(1)
     var result1 = yield asyncFun1(2)
     var e = yield [asyncFun1(5), asyncFun1(4)]
     return e;
-}).then(function(value) {
+}).then(function (value) {
     console.log(value)
-}, function(err) {
+}, function (err) {
     console.error(err.stack)
 })
 
 function asyncFun1(v) {
     return new Promise((resolve, reject) => {
-        setTimeout(function() {
+        setTimeout(function () {
             return resolve(v * 10)
         }, 500)
     })
